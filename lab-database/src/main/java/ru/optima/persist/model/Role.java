e31b72599421e70ee5821dccbb35d240ef091fd2
skopinmi <64150928+skopinmi@users.noreply.github.com>
2021-05-10 00:30:33 +0300

    И снова встроил lombok
    Что происходит?

--- lab-database/src/main/java/ru/optima/persist/model/Role.java
+++ lab-database/src/main/java/ru/optima/persist/model/Role.java
@@ -22,39 +22,4 @@ public class Role implements Serializable {
     @Column(name = "name", unique = true, nullable = false)
     private String name;
 
-//    public Role() {
-//    }
-//
-//    public Role(String name) {
-//        this.name = name;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Role role = (Role) o;
-//        return name.equals(role.name);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(name);
-//    }
 }
