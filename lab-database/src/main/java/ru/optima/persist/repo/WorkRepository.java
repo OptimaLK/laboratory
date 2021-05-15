8edb224e54b8c25c05e8ae90641d7b87b9594441
skopinmi <64150928+skopinmi@users.noreply.github.com>
2021-05-15 15:22:13 +0300

    Update WorkRepository.java
    Добавлен метод выведения списка работ для пользоваталя по его  id

--- lab-database/src/main/java/ru/optima/persist/repo/WorkRepository.java
+++ lab-database/src/main/java/ru/optima/persist/repo/WorkRepository.java
@@ -1,10 +1,17 @@
 package ru.optima.persist.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.optima.persist.model.Work;
 
+import java.util.List;
+
 @Repository
 public interface WorkRepository extends JpaRepository<Work, Long> {
+
     Work findById(long id);
+
+    @Query( "select w from Work w join w.users u where u.id = ?1")
+    public List<Work> findAllWorksByUserId(Long id);
 }
