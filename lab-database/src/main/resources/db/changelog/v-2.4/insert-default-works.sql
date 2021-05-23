4c301c7caac0c7f0df91b961d3dcd81abad1750f
skopinmi <64150928+skopinmi@users.noreply.github.com>
2021-05-23 09:43:32 +0300

    Промежуточный уже рабочий вариант
    в работу добавлен статус в ENUM

--- lab-database/src/main/resources/db/changelog/v-2.4/insert-default-works.sql
+++ lab-database/src/main/resources/db/changelog/v-2.4/insert-default-works.sql
@@ -1,8 +1,8 @@
-INSERT INTO `works` (`client_name`, `customer`, `number_contract`, `object_name`, `Registration_date`, `actual`)
-VALUES ('11', '11', '11', '11', '2021-01-01', true ),
-       ('22', '22', '22', '22', '2021-01-01', true ),
-       ('33', '33', '33', '33', '2021-01-01', false ),
-       ('44', '44', '44', '44', '2021-01-01', false );
+INSERT INTO `works` (`client_name`, `customer`, `number_contract`, `object_name`, `Registration_date`, `status`)
+VALUES ('11', '11', '11', '11', '2021-01-01', '0' ),
+       ('22', '22', '22', '22', '2021-01-01', '0' ),
+       ('33', '33', '33', '33', '2021-01-01', '2' ),
+       ('44', '44', '44', '44', '2021-01-01', '3' );
 GO
 
 INSERT INTO `users_works`(`user_id`, `work_id`)
@@ -11,7 +11,7 @@ VALUE
 ((SELECT id FROM `users` WHERE `last_name` = 'executor'), (SELECT id FROM `works` WHERE `client_name` = '22')),
 ((SELECT id FROM `users` WHERE `last_name` = 'executor'), (SELECT id FROM `works` WHERE `client_name` = '33')),
 ((SELECT id FROM `users` WHERE `last_name` = 'executor3'), (SELECT id FROM `works` WHERE `client_name` = '11')),
-((SELECT id FROM `users` WHERE `last_name` = 'executor2'), (SELECT id FROM `works` WHERE `client_name` = '44')),
+((SELECT id FROM `users` WHERE `last_name` = 'executor'), (SELECT id FROM `works` WHERE `client_name` = '44')),
 ((SELECT id FROM `users` WHERE `last_name` = 'executor1'), (SELECT id FROM `works` WHERE `client_name` = '22'));
 
 
