469915244d4453eed6cda964e942bb3a5df3e17f
skopinmi <64150928+skopinmi@users.noreply.github.com>
2021-05-24 21:29:22 +0300

    Update insert-default-works-status.sql
    исправил ошибку

--- lab-database/src/main/resources/db/changelog/v-2.5/insert-default-works-status.sql
+++ lab-database/src/main/resources/db/changelog/v-2.5/insert-default-works-status.sql
@@ -2,14 +2,14 @@ INSERT INTO `work_statuses` (`name`)
     VALUE ('NEW'), ('ON_CHECK'), ('COMPLETED');
 GO
 
-UPDATE `works` SET `workStatus_id` = (SELECT id FROM `work_statuses` WHERE `name` = 'NEW') WHERE (`client_name` = '11');
+UPDATE `works` SET `work_status_id` = (SELECT id FROM `work_statuses` WHERE `name` = 'NEW') WHERE (`client_name` = '11');
 GO
 
-UPDATE `works` SET `workStatus_id` = (SELECT id FROM `work_statuses` WHERE `name` = 'ON_CHECK') WHERE (`client_name` = '22');
+UPDATE `works` SET `work_status_id` = (SELECT id FROM `work_statuses` WHERE `name` = 'ON_CHECK') WHERE (`client_name` = '22');
 GO
 
-UPDATE `works` SET `workStatus_id` = (SELECT id FROM `work_statuses` WHERE `name` = 'COMPLETED') WHERE (`client_name` = '33');
+UPDATE `works` SET `work_status_id` = (SELECT id FROM `work_statuses` WHERE `name` = 'COMPLETED') WHERE (`client_name` = '33');
 GO
 
-UPDATE `works` SET `workStatus_id` = (SELECT id FROM `work_statuses` WHERE `name` = 'ON_CHECK') WHERE (`client_name` = '44');
+UPDATE `works` SET `work_status_id` = (SELECT id FROM `work_statuses` WHERE `name` = 'ON_CHECK') WHERE (`client_name` = '44');
 GO
