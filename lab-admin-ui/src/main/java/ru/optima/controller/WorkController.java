45ab8f9098d9fe5c87ef300cb166f46de5092b52
Misha Glushak <flatron_777@mail.ru>
2021-06-08 14:55:30 +1000

    Реализация редактирования работ на странице "Список работ" без перехода на страницу редактирования

--- lab-admin-ui/src/main/java/ru/optima/controller/WorkController.java
+++ lab-admin-ui/src/main/java/ru/optima/controller/WorkController.java
@@ -37,6 +37,7 @@ public class WorkController {
     public String workPage(Model model, SecurityContextHolder auth, Principal principal) {
         model.addAttribute("activePage", "Works");
         model.addAttribute("users", userService.findAllUserWhoHasRole("ROLE_EXECUTOR"));
+        model.addAttribute("workEdit", new WorkRepr());
 
 //      для каждой роли (роль маленькими буквами) пользователя на клиент передается свой набор данных
         switch (pathCreator.getRole(auth)) {
@@ -141,5 +142,16 @@ public class WorkController {
         work.setWorkStatus(workStatusService.findByName("COMPLETED"));
         workService.save(work);
         response.sendRedirect(request.getHeader("referer"));
+    }
+
+    @GetMapping("/editSave")
+    public String editSave(@ModelAttribute("workEdit") WorkRepr workEdit) {
+        WorkRepr workRepr = workService.findById(workEdit.getId()).orElseThrow(NotFoundException::new);
+        workRepr.setClientName(workEdit.getClientName());
+        workRepr.setObjectName(workEdit.getObjectName());
+        workRepr.setNumberContract(workEdit.getNumberContract());
+        workRepr.setAdditionalInformation(workEdit.getAdditionalInformation());
+        workService.save(workRepr);
+        return "redirect:/work";
     }
 }
